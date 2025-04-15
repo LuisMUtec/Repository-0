a6469c8b25be2319cdcc0b4d5955452b041dca80
frances <frances@forsyth.com.pe>
2025-04-15 17:42:37 -0500

    Agregar calculadora con función de suma

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Calculadora básica")
+    while True:
+        entrada = input("Ingresa operación (ej. 2 + 2) o 'c' para limpiar, 'q' para salir: ")
+
+        if entrada.lower() == 'q':
+            break
+        elif entrada.lower() == 'c':
+            continue
+
+        try:
+            resultado = eval(entrada)
+            print("Resultado:", resultado)
+        except Exception as e:
+            print("Error:", e)
+
